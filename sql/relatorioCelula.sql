acded067d646c4c302ed733de765a411e3b313a4
Wouerner <wouerner@gmail.com>
2014-03-07 14:22:04 -0300

    Removendo arquivos de documentação da bibliotecas

--- sql/relatorioCelula.sql
+++
@@ -1,8 +0,0 @@
-select c.id AS celulaID, c.nome AS celulaNome, r.titulo, r.dataEnvio, r.id AS id
-						from 
-						Celula AS c left join RelatorioCelula AS r 
-						on 
-						c.id = r.celulaId and r.dataEnvio between '2013-02-18 00:00:00' and '2013-02-18 23:59:59'
-						WHERE 1
-						group by c.id
-						order by  r.titulo
