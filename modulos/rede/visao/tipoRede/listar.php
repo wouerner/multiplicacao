11c94eca0f0f110c68781137475037a6ba6cac20
wouerner <wouerner@casa.(none)>
2014-05-12 11:43:23 -0300

    Adicionando configurações dos graficos

--- modulos/rede/visao/tipoRede/listar.php
+++ modulos/rede/visao/tipoRede/listar.php
@@ -28,7 +28,6 @@ unset($_SESSION['mensagem']) ;
                 <?php $crescimento = $rede->crescimento('2014-04-15',date('Y-m-d'))?>
                 <?php //var_dump($rede->crescimento('2014-04-15',date('2014-04-16')))?>
                 <div class = "span5 well" >
-                    <script type="text/javascript" src="/incluidos/js/rede.js"></script>
                         <h2>
                             <a href="/rede/rede/detalharTipoRede/id/<?php echo $rede->id?>" >
                                 <?php echo $rede->nome ; ?>
@@ -43,6 +42,66 @@ unset($_SESSION['mensagem']) ;
                             <a href="/rede/rede/excluirTipoRede/id/<?php echo $rede->id ; ?>" class = "btn btn-mini btn-danger" ><i class = "icon-remove icon-white" ></i> excluir</a>
                         <?php endif ; ?>
                         <script>
+
+                            var defaults = {
+                            //Boolean - If we show the scale above the chart data
+                            scaleOverlay : false,
+                            //Boolean - If we want to override with a hard coded scale
+                            scaleOverride : true,
+                            //** Required if scaleOverride is true **
+                            //Number - The number of steps in a hard coded scale
+                            scaleSteps : 40,
+                            //Number - The value jump in the hard coded scale
+                            scaleStepWidth : 2,
+                            //Number - The scale starting value
+                            scaleStartValue : <?php echo $rede->totalDiscipulosPorRede()?>,
+                            //String - Colour of the scale line
+                            scaleLineColor : "rgba(0,0,0,.1)",
+                            //Number - Pixel width of the scale line
+                            scaleLineWidth : 1,
+                            //Boolean - Whether to show labels on the scale
+                            scaleShowLabels : true,
+                            //Interpolated JS string - can access value
+                            scaleLabel : "<%=value%>",
+                            //scaleLabel : 1,
+                            //String - Scale label font declaration for the scale label
+                            scaleFontFamily : "'Arial'",
+                            //Number - Scale label font size in pixels
+                            scaleFontSize : 8,
+                            //String - Scale label font weight style
+                            scaleFontStyle : "normal",
+                            //String - Scale label font colour
+                            scaleFontColor : "#666",
+                            ///Boolean - Whether grid lines are shown across the chart
+                            scaleShowGridLines : true,
+                            //String - Colour of the grid lines
+                            scaleGridLineColor : "rgba(0,0,0,.05)",
+                            //Number - Width of the grid lines
+                            scaleGridLineWidth : 1,
+                            //Boolean - Whether the line is curved between points
+                            bezierCurve : true,
+                            //Boolean - Whether to show a dot for each point
+                            pointDot : true,
+                            //Number - Radius of each point dot in pixels
+                            pointDotRadius : 3,
+                            //Number - Pixel width of point dot stroke
+                            pointDotStrokeWidth : 1,
+                            //Boolean - Whether to show a stroke for datasets
+                            datasetStroke : true,
+                            //Number - Pixel width of dataset stroke
+                            datasetStrokeWidth : 2,
+                            //Boolean - Whether to fill the dataset with a colour
+                            datasetFill : true,
+                            //Boolean - Whether to animate the chart
+                            animation : true,
+                            //Number - Number of animation steps
+                            animationSteps : 60,
+                            //String - Animation easing effect
+                            animationEasing : "easeOutQuart",
+                            //Function - Fires when the animation is complete
+                            onAnimationComplete : null
+                            }
+
                             var data = {
                                 labels : [
                                             <?php echo '"'.implode('","',array_keys($crescimento)).'"'?>
