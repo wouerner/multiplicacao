b9bf5bf193a38824d2624709126edf95d0a1812e
wouerner <wouerner@casa.(none)>
2014-05-12 11:31:24 -0300

    Adicionando nova visão para redes

--- modulos/rede/modelo/tipoRede.php
+++ modulos/rede/modelo/tipoRede.php
@@ -206,4 +206,42 @@ class tipoRede extends modeloFramework
             $this->erro= $e->getMessage();
         }
     }
+
+    public function crescimento($inicio, $fim)
+    {
+        $begin = new \DateTime( $inicio );
+        $end = new \DateTime( $fim );
+       // $end = $end->modify( '+1 day' );
+
+        $interval = new \DateInterval('P1D');
+        $daterange = new \DatePeriod($begin, $interval ,$end);
+        //var_dump($daterange);
+        $select='';
+        $select2='';
+
+        foreach ($daterange as $date) {
+            $select .= "q.`{$date->format('d-m-Y')}`,";
+        }
+        $select = substr($select,0,strlen($select)-1);
+
+        foreach ($daterange as $date) {
+            $select2 .= "SUM(IF(DATE_FORMAT(data,'%Y-%m-%d') = '{$date->format('Y-m-d')}',total,0)) AS '{$date->format('d-m-Y')}',";
+        }
+        $select2 = substr($select2,0,strlen($select2)-1);
+
+        //var_dump($select);
+        //var_dump($select2);
+        $sql = 'select '.$select.' from ( select  '.$select2.' from  QtdDiscipulosRede where redeId = '.$this->id.') as q';
+        //echo $sql;
+        //exit;
+
+            $pdo = new \PDO(DSN,USER,PASSWD);
+
+            $stm = $pdo->prepare($sql);
+
+            $stm->execute();
+
+            return $stm->fetch(\PDO::FETCH_ASSOC);
+            //return $stm->fetchAll();
+    }
 }
--- modulos/rede/visao/tipoRede/listar.php
+++ modulos/rede/visao/tipoRede/listar.php
@@ -1,7 +1,6 @@
 <?php
 $mensagem = isset($_SESSION['mensagem']) ? $_SESSION['mensagem'] : NULL ;
 unset($_SESSION['mensagem']) ;
-
 ?>
 <!DOCTYPE html>
 <html>
@@ -10,20 +9,68 @@ unset($_SESSION['mensagem']) ;
         <?php include 'incluidos/css.inc.php' ?>
         <?php include 'incluidos/js.inc.php' ?>
     </head>
-
     <body>
         <section class = "container-fluid">
-
         <nav>
-
             <?php include 'modulos/menu/visao/menu.inc.php' ; ?>
         </nav>
-
+        <section>
+            <style>
+                #chart {
+                    width: 100% !important;
+                    /*max-width: 800px;*/
+                    height: auto !important;
+                }
+            </style>
+            <div class="row-fluid">
+                <h1>Redes</h1>
+                <?php foreach ( $redes as $rede) : ?>
+                <?php $crescimento = $rede->crescimento('2014-04-15',date('Y-m-d'))?>
+                <?php //var_dump($rede->crescimento('2014-04-15',date('2014-04-16')))?>
+                <div class = "span5 well" >
+                    <script type="text/javascript" src="/incluidos/js/rede.js"></script>
+                        <h2>
+                            <a href="/rede/rede/detalharTipoRede/id/<?php echo $rede->id?>" >
+                                <?php echo $rede->nome ; ?>
+                            </a>
+                        </h2>
+                        <canvas id="chart<?php echo $rede->id?>" width="500"  height="300"></canvas>
+                        <span class="label label-info"><?php echo $rede->totalDiscipulosPorRede() ; ?></span>
+                        <a href="/rede/rede/listarMembrosRede/id/<?php echo $rede->id ; ?>" class = "btn btn-mini " ><i class = "icon-list" ></i> listar Discipulos </a>
+                        <a href="/rede/rede/listarCelulas/id/<?php echo $rede->id ; ?>" class = "btn btn-mini " ><i class = "icon-list" ></i> listar Células</a>
+                        <?php if ($acesso->hasPermission('admin_acesso') == true): ?>
+                            <a href="/rede/rede/atualizarTipoRede/id/<?php echo $rede->id ; ?>" class = "btn btn-mini btn-primary" ><i class="icon-edit icon-white"></i> Atualizar</a>
+                            <a href="/rede/rede/excluirTipoRede/id/<?php echo $rede->id ; ?>" class = "btn btn-mini btn-danger" ><i class = "icon-remove icon-white" ></i> excluir</a>
+                        <?php endif ; ?>
+                        <script>
+                            var data = {
+                                labels : [
+                                            <?php echo '"'.implode('","',array_keys($crescimento)).'"'?>
+                                        ],
+                                datasets : [
+                                    {
+                                    fillColor : "rgba(250,0,0,0.5)",
+                                    strokeColor : "rgba(220,0,0,1)",
+                                    pointColor : "rgba(220,220,220,1)",
+                                    pointStrokeColor : "#fff",
+                                        data : [
+                                            <?php echo implode(',',array_values($crescimento))?>
+                                            ]
+                                    },
+                                ]
+                            }
+                            var ctx = $("#chart<?php echo $rede->id?>").get(0).getContext("2d");
+                            //var ctx = document.getElementById("#chart<?php echo $rede->id?>").getContext("2d");
+                            console.log(ctx);
+                            var myNewChart = new Chart(ctx).Line(data,defaults);
+                        </script>
+                </div>
+                <?php endforeach ; ?>
+            </div>
+        </section>
         <section>
             <article>
-
                 <?php require 'modulos/discipulo/visao/chamarDiscipulo.php' ; ?>
-
                 <div class = "row-fluid" >
                 <div class = "span12" >
                         <table class = "well table table-condensed">
@@ -44,7 +91,8 @@ unset($_SESSION['mensagem']) ;
 
                         <tr>
                             <td><?php echo !isset($c) ? $c=1 : ++$c ; ?></td>
-                            <td><a href="/rede/rede/detalharTipoRede/id/<?php echo $rede->id?>" ><?php echo $rede->nome ; ?> </a></td>
+                            <td><a href="/rede/rede/detalharTipoRede/id/<?php echo $rede->id?>" >
+                                <?php echo $rede->nome ; ?> </a></td>
                             <td><?php echo $rede->totalDiscipulosPorRede() ; ?> </td>
                             <td><?php echo $rede->getMeta() ; ?> </td>
                             <?php $totalMeta += $rede->getMeta() ; ?>
