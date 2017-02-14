e190d950dc6ea9b269ab3f89b69ee267a2111624
wouerner <wouerner@gmail.com>
2017-02-14 10:46:20 -0200

    [FIX]

--- modulos/EncontroComDeus/visao/participantesEncontro/lista2.php
+++ modulos/EncontroComDeus/visao/participantesEncontro/lista2.php
@@ -36,6 +36,7 @@
                                 <tr class = "<?php echo $d->desistiu==1 ? 'error' : ''; ?>" >
                                     <td><?php echo !isset($c) ? $c=1 : ++$c ; ?></td>
                                     <td><?php echo $d->nome ; ?></td>
+                                    <td><?php echo !empty($d->getLider()) ? $d->getLider()->getAlcunha() : null ; ?></td>
                                 </tr>
                                 <?php endforeach ; ?>
                         </table>
