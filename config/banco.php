e7d3ba948cc3a186043e4e388e02cf012bf1687e
wouerner <wouerner@protonmail.com>
2020-04-29 17:04:04 -0300

    config

---
+++ config/banco.php
@@ -0,0 +1,13 @@
+<?php
+/*define('DSN', 'mysql:host=mysql08.wouerner.eti.br;dbname=wouerner17');
+define('USER', 'wouerner17');
+define('PASSWD', 'bdmultimga');
+define('DB', 'wouerner17');
+define('DB_SERVER', 'mysql08.wouerner.eti.br');*/
+
+define('DSN', 'mysql:host=db;dbname=multiplicacao');
+define('USER', 'root');
+define('PASSWD', 'root');
+define('DB', 'multiplicacao');
+define('DB_SERVER', 'db');
+?>
