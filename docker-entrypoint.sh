0c6322d77a9bd3947d0f044697aa4e981be6627b
wouerner <wouerner@protonmail.com>
2020-04-29 15:51:03 -0300

    novo banco de dados

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 set -e
+
+if [ ! -d "vendor" ] 
+then
+    echo "instalando dependencias"
+else
+    echo "dependencias já instaladas"
+fi
+
+
 echo "[ ****************** ] Starting Endpoint of Application"
+rm -f /var/run/apache2/apache2.pid
 apache2ctl -D FOREGROUND
 echo "[ ****************** ] Ending Endpoint of Application"
 exec "$@"
